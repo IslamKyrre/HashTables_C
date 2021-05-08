f3fbf8e2f9f87617d34e84786cab8ccb53c57166
Islam <islamxx@mail.ru>
2021-05-08 15:23:58 +0300

    Добавлена первая серия юнит-тестов.

--- tests.c
+++ tests.c
@@ -5,7 +5,82 @@
 
 
 int main() {
-    printf("Here will be tests\n");
+    HASHTABLES_ERR err;
+    HashTable *table = NULL;
+
+
+
+    table = init_hashtable(4, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    insert_key(1, table, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    int find_res = find_key(1, table, &err);
+    if (!find_res || err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    find_res = find_key(2, table, &err);
+    if (find_res || err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    insert_key(10, NULL, &err);
+    if (err != EINVARG)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    insert_key(9, table, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    insert_key(2, table, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+
+    remove_key(3, table, &err);
+    if (err != EREMOVE)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+    remove_key(9, table, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
+
+
+    remove_hashtable(table, &err);
+    if (err != ESUCCESS)
+        fprintf(stdout, "Test\t->\tFAILED\n");
+    else
+        fprintf(stdout, "Test\t->\tPASSED\n");
+
 
     return 0;
 }
